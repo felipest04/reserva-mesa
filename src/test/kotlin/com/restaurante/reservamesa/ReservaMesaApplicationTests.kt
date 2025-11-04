9d09cdd1486873618f8deddc62c8b460fb5a1d6e
felipest04 <santosofeliipe@gmail.com>
2025-11-03 22:20:40 -0300

    init

---
+++ src/test/kotlin/com/restaurante/reservamesa/ReservaMesaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.restaurante.reservamesa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservaMesaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
